09ed1517cea9ad8016819667ed54b465cce48562
24duong@gmail.com <Haiduong24>
2017-07-02 23:58:19 +0700

    xong lay du lieu vnexpress

--- config/News.js
+++ config/News.js
@@ -1,149 +1,230 @@
 var VnExpress = {
-    ID: '001',
+    IDNews: '001',
     NewsName: 'VnExpress',
     Logo: 'https://s.vnecdn.net/vnexpress/i/v20/logos/vne_logo_rss.png',
     arrRss: [{
+
+            IDCategory: 'VnExpressCG001',
             Category: 'Tin mới nhất',
             LinkCategory: 'http://vnexpress.net/rss/tin-moi-nhat.rss'
         },
         {
+            IDCategory: 'VnExpressCG002',
             Category: 'Thời sự',
             LinkCategory: 'http://vnexpress.net/rss/thoi-su.rss'
         },
         {
+            IDCategory: 'VnExpressCG003',
             Category: 'Thế Giới',
             LinkCategory: 'http://vnexpress.net/rss/the-gioi.rss'
         },
         {
+            IDCategory: 'VnExpressCG004',
             Category: 'Kinh Doanh',
             LinkCategory: 'http://vnexpress.net/rss/kinh-doanh.rss'
         },
         {
+            IDCategory: 'VnExpressCG005',
             Category: 'Startup',
             LinkCategory: 'http://vnexpress.net/rss/startup.rss'
         },
         {
+            IDCategory: 'VnExpressCG006',
             Category: 'Giải trí',
             LinkCategory: 'http://vnexpress.net/rss/giai-tri.rss'
         },
         {
+            IDCategory: 'VnExpressCG007',
             Category: 'Thể thao',
             LinkCategory: 'http://vnexpress.net/rss/the-thao.rss'
         },
         {
+            IDCategory: 'VnExpressCG008',
             Category: 'Pháp luật',
             LinkCategory: 'http://vnexpress.net/rss/phap-luat.rss'
         },
         {
+            IDCategory: 'VnExpressCG008',
             Category: 'Giáo dục',
             LinkCategory: 'http://vnexpress.net/rss/giao-duc.rss'
         },
         {
+            IDCategory: 'VnExpressCG010',
             Category: 'Sức khỏe',
             LinkCategory: 'http://vnexpress.net/rss/suc-khoe.rss'
         },
         {
+            IDCategory: 'VnExpressCG011',
             Category: 'Gia đình',
             LinkCategory: 'http://vnexpress.net/rss/gia-dinh.rss'
         },
         {
+            IDCategory: 'VnExpressCG012',
             Category: 'Du lịch',
             LinkCategory: 'http://vnexpress.net/rss/du-lich.rss'
         },
         {
+            IDCategory: 'VnExpressCG013',
             Category: 'Khoa học',
             LinkCategory: 'http://vnexpress.net/rss/khoa-hoc.rss'
         },
         {
+            IDCategory: 'VnExpressCG014',
             Category: 'Số hóa',
             LinkCategory: 'http://vnexpress.net/rss/so-hoa.rss'
         },
         {
+            IDCategory: 'VnExpressCG015',
             Category: 'Xe',
             LinkCategory: 'http://vnexpress.net/rss/oto-xe-may.rss'
         },
         {
+            IDCategory: 'VnExpressCG016',
             Category: 'Cộng đồng',
             LinkCategory: 'http://vnexpress.net/rss/cong-dong.rss'
         },
         {
+            IDCategory: 'VnExpressCG017',
             Category: 'Tâm sự',
             LinkCategory: 'http://vnexpress.net/rss/tam-su.rss'
         },
         {
+            IDCategory: 'VnExpressCG018',
             Category: 'Cười',
             LinkCategory: 'http://vnexpress.net/rss/cuoi.rss'
         },
     ],
 }
+
+var ItemVnExpress = {
+    title: "Quảng Ngãi đình chỉ 10 canô chở khách qua đảo Bé, Lý Sơn",
+    description: "<a href=\"http://vnexpress.net/tin-tuc/thoi-su/quang-ngai-dinh-chi-10-cano-cho-khach-qua-dao-be-ly-son-3607732.html\"><img width=130 height=100 src=\"http://img.f29.vnecdn.net/2017/07/02/daobe-1498992228_180x108.jpg\" ></a></br>Các canô chở khách từ đảo Lớn sang đảo Bé (đảo Lý Sơn, Quảng Ngãi) bị đình chỉ do không đạt chuẩn, tiềm ẩn nguy cơ cho du khách.",
+    pubDate: "Sun, 02 Jul 2017 18:02:42 +0700",
+    link: "http://vnexpress.net/tin-tuc/thoi-su/quang-ngai-dinh-chi-10-cano-cho-khach-qua-dao-be-ly-son-3607732.html",
+    contentHtml: "",
+    linkImg: ["", ""]
+}
+var ItemVnExpress = {
+    IDCategory: 'VnExpressCG001',
+    title: "Quảng Ngãi đình chỉ 10 canô chở khách qua đảo Bé, Lý Sơn",
+    descriptionImage: "",
+    descriptionText: "<a href=\"http://vnexpress.net/tin-tuc/thoi-su/quang-ngai-dinh-chi-10-cano-cho-khach-qua-dao-be-ly-son-3607732.html\"><img width=130 height=100 src=\"http://img.f29.vnecdn.net/2017/07/02/daobe-1498992228_180x108.jpg\" ></a></br>Các canô chở khách từ đảo Lớn sang đảo Bé (đảo Lý Sơn, Quảng Ngãi) bị đình chỉ do không đạt chuẩn, tiềm ẩn nguy cơ cho du khách.",
+    pubDate: "Sun, 02 Jul 2017 18:02:42 +0700",
+    link: "http://vnexpress.net/tin-tuc/thoi-su/quang-ngai-dinh-chi-10-cano-cho-khach-qua-dao-be-ly-son-3607732.html",
+    contentHead: `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        img,
+        video {
+            width: 100%
+        }
+        
+        .block_timer_share {
+            display: none;
+        }
+        
+        .social_share {
+            display: none;
+        }
+    </style>
+</head>
+
+<body>`,
+    contentFoot: `</body>
+
+</html>`,
+    contentBody: "",
+    linkImg: ["", ""]
+}
+
 var TuoiTre = {
-    ID: '002',
+    IDNews: '002',
     NewsName: 'Tuổi Trẻ',
     Logo: 'http://static.new.tuoitre.vn/tuoitre.png',
     arrRss: [{
             LinkCategory: "http://tuoitre.vn/rss/tt-tin-moi-nhat.rss",
+            IDCategory: '',
             Category: "Trang chủ"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-chinh-tri-xa-hoi.rss",
+            IDCategory: '',
             Category: "Chính trị - Xã hội"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-the-gioi.rss",
+            IDCategory: '',
             Category: "Thế giới"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-phap-luat.rss",
+            IDCategory: '',
             Category: "Pháp luật"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-kinh-te.rss",
+            IDCategory: '',
             Category: "Kinh tế"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-song-khoe.rss",
+            IDCategory: '',
             Category: "Sống khỏe"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-giao-duc.rss",
+            IDCategory: '',
             Category: "Giáo dục"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-the-thao.rss",
+            IDCategory: '',
             Category: "Thể thao"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-van-hoa-giai-tri.rss",
+            IDCategory: '',
             Category: "Văn hóa - Giải trí"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-nhip-song-tre.rss",
+            IDCategory: '',
             Category: "Nhịp sống trẻ"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-nhip-song-so.rss",
+            IDCategory: '',
             Category: "Nhịp sống số"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-ban-doc.rss",
+            IDCategory: '',
             Category: "Bạn đọc"
         },
         {
             LinkCategory: "http://tuoitre.vn/rss/tt-du-lich.rss",
+            IDCategory: '',
             Category: "Du lịch"
         },
 
     ],
 }
 var Bao24h = {
-    ID: '003',
+    IDNews: '003',
     NewsName: 'Báo 24h',
     Logo: 'http://anh.24h.com.vn/upload/footer/2009-12-02/20091202162630_logo-chan-trang-24h.jpg',
     arrRss: [{
+            IDCategory: '',
             Category: "Tin tức mới nhất",
             LinkCategory: "http://www.24h.com.vn/upload/rss/trangchu24h.rss"
         },
         {
+            IDCategory: '',
             Category: "Tin tức trong ngày",
 
 
@@ -154,6 +235,7 @@ var Bao24h = {
             LinkCategory: "http://www.24h.com.vn/upload/rss/tintuctrongngay.rss",
         },
         {
+            IDCategory: '',
             Category: "Bóng đá",
 
 
@@ -164,6 +246,7 @@ var Bao24h = {
             LinkCategory: "http://www.24h.com.vn/upload/rss/bongda.rss",
         },
         {
+            IDCategory: '',
             Category: "Euro 2016",
 
 
@@ -174,6 +257,7 @@ var Bao24h = {
             LinkCategory: "http://www.24h.com.vn/upload/rss/euro2016.rss",
         },
         {
+            IDCategory: '',
             Category: "An ninh - Hình sự",
 
 
@@ -186,6 +270,7 @@ var Bao24h = {
         },
         {
 
+            IDCategory: '',
             Category: "Thời trang",
 
 
@@ -199,6 +284,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Thời trang Hi-tech",
 
 
@@ -212,6 +298,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Tài chính – Bất động sản",
 
 
@@ -225,6 +312,7 @@ var Bao24h = {
         },
         {
 
+            IDCategory: '',
             Category: "Ẩm thực",
 
 
@@ -239,6 +327,7 @@ var Bao24h = {
         },
         {
 
+            IDCategory: '',
             Category: "Làm đẹp",
 
 
@@ -253,6 +342,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Phim",
 
 
@@ -269,6 +359,7 @@ var Bao24h = {
         {
 
 
+            IDCategory: '',
             Category: "Giáo dục - du học",
 
 
@@ -282,6 +373,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Bạn trẻ - Cuộc sống",
 
 
@@ -296,6 +388,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Ca nhạc - MTV",
 
 
@@ -309,6 +402,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Thể thao",
 
 
@@ -321,6 +415,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Phi thường - kỳ quặc",
 
 
@@ -334,6 +429,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Công nghệ thông tin",
 
 
@@ -348,6 +444,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Ô tô - Xe máy",
 
 
@@ -360,6 +457,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Thị trường - Tiêu dùng",
 
 
@@ -374,6 +472,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Du lịch",
 
 
@@ -387,6 +486,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Sức khỏe đời sống",
 
 
@@ -399,6 +499,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Cười 24h",
 
 
@@ -412,6 +513,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Thế giới",
 
 
@@ -425,6 +527,7 @@ var Bao24h = {
         },
         {
 
+            IDCategory: '',
             Category: "Đời sống Showbiz",
 
 
@@ -438,6 +541,7 @@ var Bao24h = {
 
         },
         {
+            IDCategory: '',
             Category: "Giải trí",
 
 
--- obj/readHTML.js
+++ obj/readHTML.js
@@ -1,8 +1,28 @@
 var jsdom = require("jsdom/lib/old-api.js");
 
 jsdom.env(
-    "http://video.vnexpress.net/tin-tuc/xa-hoi/treo-cay-co-thu-lay-nam-cho-thu-tien-trieu-moi-ngay-3607414.html", ["http://code.jquery.com/jquery.js"],
+    "http://video.vnexpress.net/tin-tuc/phap-luat/luat-su-phuong-nga-ke-chuyen-bi-than-chu-tham-van-nguoc-3607231.html", ["http://code.jquery.com/jquery.js"],
     function(err, window) {
-        console.log("there have been", window.$("#left_calculator").html(), "io.js releases!");
+        // var content = window.$("#left_calculator").html();
+        try {
+            var content = window.$("script").text().split("VideoVNE.config_play")[1].split("};")[0];
+            content = "config_play " + content + "};";
+            console.log(content)
+            var content = window.$("script").text().split("s720: '")[1].split("',")[0];
+            console.log(content)
+            var content = window.$("script").text().split("s480: '")[1].split("',")[0];
+            console.log(content)
+        } catch (error) {
+            var content = window.$("script").text().split("s480: '")[1].split("',")[0];
+            console.log(content)
+        }
+
+
+
+        // if (content == undefined) {
+        //     content = window.$("#media-video").attr("src");
+        //     console.log(content)
+        // }
+
     }
 );
